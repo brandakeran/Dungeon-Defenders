29f840f01bcc214e56cbebdf57bd7ffed4873bed
Brandon Chen <brandon.chen1016@gmail.com>
2020-05-08 04:36:40 -0400

    Movement
    Movement and third person camera system (excluding jumps)

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -7,10 +7,12 @@ public class EnemyHealth : MonoBehaviour
     public int totalHealth = 100;
     public int currentHealth;
     bool isDead;
+    public EnemySpawn spawner;
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = totalHealth;
+        spawner = GameObject.Find("EnemySpawner").GetComponent<EnemySpawn>();
     }
 
     // Update is called once per frame
@@ -30,6 +32,8 @@ public class EnemyHealth : MonoBehaviour
 
     void Death()
     {
-
+        print(spawner);
+        spawner.numberOfEnemies--;
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+
+    public int damage = 100;
+    public float range = 300f;
+    public float timeBetweenShots = 0.5f;
+
+    float timer;
+    Ray shootRay;
+    RaycastHit shootHit;
+
+    LineRenderer gunLine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gunLine = GetComponent<LineRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (Input.GetButton("Fire1") && timer >= timeBetweenShots)
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        timer = 0f;
+
+        gunLine.enabled = true;
+        gunLine.SetPosition(0, transform.position);
+
+        shootRay.origin = transform.position;
+        shootRay.direction = transform.forward;
+
+        if(Physics.Raycast(shootRay, out shootHit, range))
+        {
+            EnemyHealth enemyHealth = shootHit.collider.GetComponent<EnemyHealth>();
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(damage);
+            }
+            gunLine.SetPosition(1, shootHit.point);
+        }
+        else
+        {
+            gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+        }
+
+    }
+}
